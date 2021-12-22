1f6b8b7133320d79860ce3ee4d088d664c08bf3a
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-12-23 05:04:13 +0530

    Added: File With UC1

--- src/Employee_Wage.php
+++ src/Employee_Wage.php
@@ -1,2 +1,22 @@
 <?php
-echo "Welcome to Employee Wage Computation Problem";
+echo "Welcome to Employee Wage Computation Problem\n";
+// Class to Compute Employee Wage Problem
+class Employee_Wage
+{
+    /**
+     * Function to Check Employee is Present or Absent
+     * Non-Parameterized function
+     * Using rand() to generate the random of 0 or 1
+     */
+    function attendance()
+    {
+        $empCheck = rand(0, 1);
+        if ($empCheck == 1) {
+            echo "Employee is Present";
+        } else {
+            echo "Employee is Absent";
+        }
+    }
+}
+$obj = new Employee_Wage();
+$obj->attendance();
